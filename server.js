dcc9fa284cfa4db26b396285038492020b7d94f3
Syrup <admin@mioun.my.id>
2021-01-08 20:51:45 +0700

    Add New API

--- server.js
+++ server.js
@@ -6,26 +6,31 @@ module.exports = class MiounApi {
     this.author = author;
     this.version = version;
   }
-async github(name) {
-  const gname = encodeURI(name);
-  const { body: github } = await fetch.get(`https://api.github.com/users/${gname}`);
-  return github;
-};
-async kiss() {
-  const { body: kiss } = await fetch.get('https://nekos.life/api/kiss')
-  return kiss;
-};
-async hug() {
-  const { body: hug } = await fetch.get('https://nekos.life/api/hug');
-  return hug;
-}
-async anime() {
-  const { body: anime } = await fetch.get('https://nezumiapi.veguiizumi.repl.co/api/anime')
-  return anime;
-}
-async aes(text) {
-  const atext = encodeURI(text);
-  const {  body: aesthetic } = await fetch.get(`https://nezumiapi.veguiizumi.repl.co/api/aesthetic?text=${atext}`);
-  return aesthetic;
-}
+  async github(name) {
+    const gname = encodeURI(name);
+    const { body: github } = await fetch.get(`https://api.github.com/users/${gname}`);
+    return github;
+  };
+  syncc kiss() {
+    const { body: kiss } = await fetch.get('https://nekos.life/api/kiss')
+    return kiss;
+  };
+  async hug() {
+    const { body: hug } = await fetch.get('https://nekos.life/api/hug');
+    return hug;
+  }
+  async anime() {
+    const { body: anime } = await fetch.get('https://nezumiapi.veguiizumi.repl.co/api/anime')
+    return anime;
+  }
+  async aes(text) {
+    const atext = encodeURIComponent(text);
+    const {  body: aesthetic } = await fetch.get(`https://nezumiapi.veguiizumi.repl.co/api/aesthetic?text=${atext}`);
+    return aesthetic;
+  }
+  async trakteer(name) {
+    const tname = encodeURIComponent(name);
+    const res = await fetch.get(`https://api.mioun.my.id/trakteer?name=${tname}`)
+    return res;
+  }
 }
